5695a5e29303fb86cd507d78cba4123a9d44a636
Marcus Turnbo <marcus@emarcus.com>
2025-10-17 11:35:00 -0500

    Added debugging for database connection

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/db', function(req, res, next) {
-    console.log(`Attempting connection to MySQL on ${process.env.MYSQL_HOST}`);
+    console.log(`Attempting connection to MySQL on ${process.env.MYSQL_HOST} | ${process.env.MYSQL_USER} | ${process.env.MYSQL_DATABASE}`);
     pool.getConnection()
         .then(connection => {
             console.log(`✓ Database connected successfully`);
@@ -22,6 +22,11 @@ router.get('/db', function(req, res, next) {
             console.error('✗ Database connection failed:', err.message);
             console.error('Please check your database configuration');
         });
+});
+
+/* GET Index Page */
+router.get('/env', function(req, res, next) {
+    res.render('index', { title: 'User Messaging API' });
 });
 
 export default router;
