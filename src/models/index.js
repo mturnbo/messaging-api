15febf210c32bcfd339c7f2fd5e4cdfe012bf75f
Marcus Turnbo <marcus@emarcus.com>
2025-11-09 11:24:05 -0600

    Updated model index file

--- src/models/index.js
+++ src/models/index.js
@@ -4,17 +4,17 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+const dotenv = require("dotenv");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_TYPE,
+  logging: true,
+});
 
 fs
   .readdirSync(__dirname)
