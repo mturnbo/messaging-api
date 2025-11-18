54f4ff77978edc97d2514c82294fd47a06889a68
Marcus Turnbo <marcus@emarcus.com>
2025-11-18 09:48:00 -0600

    Updated user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,9 @@ import { authMiddleware } from "#middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get('/all/:limit/:page', authMiddleware, UserController.getAllUsers );
+router.get('/', authMiddleware, UserController.getAllUsers );
+router.get('/:limit/:page', authMiddleware, UserController.getAllUsers );
 router.get('/:id', authMiddleware, UserController.getUserById );
-router.delete('/:id', authMiddleware, UserController.deleteUser);
+router.delete('/delete/:id', authMiddleware, UserController.deleteUser);
 
 export default router;
